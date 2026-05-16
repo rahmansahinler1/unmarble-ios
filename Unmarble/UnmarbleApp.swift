9c15889c9179546fa9eee23fd666db579a784232
Rahman Sahinler <rahmansahinler1@gmail.com>
2026-05-16 19:29:55 +0300

    Initial Xcode project

---
+++ Unmarble/UnmarbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnmarbleApp.swift
+//  Unmarble
+//
+//  Created by Rahman Şahinler on 16.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct UnmarbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
